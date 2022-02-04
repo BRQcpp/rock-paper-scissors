b494f5e3557ce63be8e2f8ae593dc1e7a1f44904
BRQ <bnowak4744@gmail.com>
2022-02-04 19:15:34 +0100

    Reworked js to make it work with html

--- rpssource.js
+++ rpssource.js
@@ -1,46 +1,56 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function computerPlay() {
-    const selection =  Math.floor(Math.random() * 3);
-
-
-    if(selection === 0)
-        return "paper";
-    else if(selection === 1)
-        return "rock"
-    else 
-        return "scissors"
+    return Math.floor(Math.random() * 3);
 }
 
-
-/*function playRound(playerSelection, computerSelection) {
-    if(playerSelection === computerSelection)
-        console.log("Tie");
-    else if(playerSelection === "paper" && computerSelection === "rock")
-        console.log("Player won!")
-    else if(playerSelection === "scissors" && computerSelection === "paper")
-        console.log("Player won!")
-    else if(playerSelection === "rock" && computerSelection === "scissors")
-        console.log("Player won!")
-    else 
-        console.log("Computer won!");
-}*/
-   
-
-function playRound(playerSelection, computerSelection) {
-    if(playerSelection === computerSelection)
-        console.log("Tie");
-    else if(    (playerSelection === "paper" && computerSelection === "rock") 
-             || (playerSelection === "scissors" && computerSelection === "paper")
-             || (playerSelection === "rock" && computerSelection === "scissors")    )
-        console.log("Player won!");
-    else 
-        console.log("Computer won!");
+function displayChoice(Selection, id) 
+{   
+    switch(+Selection)
+    {
+        case 0 : document.querySelector(id).innerHTML = '<i class="icon-hand-grab-o icon"> </i>'; break;
+        case 1 : document.querySelector(id).innerHTML = '<i class="icon-hand-paper-o icon"> </i>'; break;
+        case 2 : document.querySelector(id).innerHTML = '<i class="icon-hand-scissors-o icon"> </i>'; break;
+    }
 }
 
+function playRound(playerSelection, computerSelection)
+{
+    displayChoice(computerSelection, '#computer-display');
+    displayChoice(playerSelection, '#player-display');
+
+    if(playerScore == 3 || computerScore == 3)
+        playerScore = computerScore = document.querySelector('#score-player').textContent = document.querySelector('#score-computer').textContent = 0;
+    
+    if(playerSelection == computerSelection)
+        document.querySelector('.round-info').textContent = 'Tie!';
+    else if(    (playerSelection == 0 && computerSelection == 2) 
+             || (playerSelection == 2 && computerSelection == 1)
+             || (playerSelection == 1 && computerSelection == 0)    )
+    {
+        document.querySelector('.round-info').textContent = 'Player won!';
+        document.querySelector('#score-player').textContent = +playerScore + 1;
+    }
+    else 
+    {
+        document.querySelector('.round-info').textContent = 'Computer won!';
+        document.querySelector('#score-computer').textContent = +computerScore + 1;
+    }
+
+    playerScore = document.querySelector('#score-player').textContent;
+    computerScore = document.querySelector('#score-computer').textContent;
+
+    if(playerScore == 3)
+        document.querySelector('.round-info').textContent = 'Player won game!'
+    else if(computerScore == 3)
+       document.querySelector('.round-info').textContent = 'Computer won game!'
+
+}
+
+let buttons = document.querySelectorAll('.choice');
+
+buttons.forEach( button => button.addEventListener('click', () =>  playRound(button.getAttribute('data-number'), computerPlay())) )
 
 
- 
-const playerSelection = "rock";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
-console.log(`Player selection: ${playerSelection}`);
-console.log(`Computer selection: ${computerSelection}`);
+
